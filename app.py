8cd4dcb9ad9b9c2b44af04e67ae25c22433cdf92
lindsey <jd5290@nyu.edu>
2023-09-27 17:55:03 -0400

    add requirements file

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ x=st.slider('Select a number')
 st.write(x, "squared is", x * x)
 
 # how to update the change in github
+# Git -> Commit -> Select and write notes -> Comit
+# Git -> Push -> Select the commit you want to update
